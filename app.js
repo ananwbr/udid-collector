2c084c717a6c3f34da6578fdf3822d6bb489e7e3
ananwbr <benraywu@gmail.com>
2015-12-29 10:15:13 +0800

    调整文件组织结构

--- app.js
+++ app.js
@@ -5,20 +5,25 @@ var router = require('koa-router')();
 var bodyParser = require('koa-bodyparser');
 var fileServer = require('koa-static');
 
-var integration = require('./integration');
-var udid = require('./udid');
+var integration = require('./lib/integration');
+var udid = require('./lib/udid');
 
 app.use(bodyParser());
 
 router.post('/udid', udid.collect);
+
 router.get('/show', udid.show);
 
 router.post('/create', integration.create);
+
+router.get('/udidcollector/*', function *(){
+	this.body = 'Bravo!!!';
+});
 
 app
   .use(router.routes())
   .use(router.allowedMethods());
 
-app.use(fileServer(__dirname + '/files'));
+app.use(fileServer(__dirname + '/public'));
 
 app.listen(3000);
