798a2d6829b086eb47fd21ee3cdbce9ca3e40885
summer <summer222522@gmail.com>
2016-01-07 22:18:18 +0800

    device tip text update

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp')
 var sass = require('gulp-sass')
 
 gulp.task('styles', function() {
-  gulp.src('./views/static/sass/**/*.scss')
+  gulp.src('./public/static/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./views/static/css/'))
+    .pipe(gulp.dest('./public/static/css/'))
 })
 
 gulp.task('default', function() {
-  gulp.watch('./views/static/sass/**/*.scss', ['styles'])
+  gulp.watch('./public/static/sass/**/*.scss', ['styles'])
 })
